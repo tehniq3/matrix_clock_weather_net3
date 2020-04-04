b8a2d855bfd7f36076a1fc3538135e363cced70e
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2020-04-04 19:22:12 +0300

    Update marquee1.ino

--- marquee1/marquee1.ino
+++ marquee1/marquee1.ino
@@ -19,6 +19,8 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
+  
+  Nicu Florica (niq_ro) just make small changs: dd degree symbol, wind in km/h, pressure in mmH
 */
 
 /**********************************************
